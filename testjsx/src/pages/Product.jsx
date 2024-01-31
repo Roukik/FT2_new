4309d50a717e024d4f7b821613b1d7d2524e2658
vanellazerr <148196491+vanellazerr@users.noreply.github.com>
2024-01-31 19:26:07 +0100

    nknn

--- testjsx/src/pages/Product.jsx
+++ testjsx/src/pages/Product.jsx
@@ -1,11 +1,21 @@
+
 import React from 'react';
+import TrainList from './TrainList'; 
 
 const Product = () => {
-    return (
-        <div>
-            <h1>Product page</h1>
-        </div>
-    );
+  return (
+    <div style={{ display: 'flex' }}>
+      {/* Partie gauche avec la liste des trains */}
+      <div style={{ width: '50%', padding: '20px' }}>
+        <h2>Liste de Trains</h2>
+        <TrainList />
+      </div>
+      <div style={{ width: '50%', padding: '20px', backgroundColor: '#fff', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+        <h1>Product page</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
+
